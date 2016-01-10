f17958ac533d4aea0c1536c308d867d356b0e987
lukaskwkw <lukaskwkw@gmail.com>
2016-01-10 09:05:21 +0100

    version 1.1. Added support for second link

--- public/js/index.js
+++ public/js/index.js
@@ -29,17 +29,18 @@ class Table extends React.Component {
 		this.setRows(arr);
 	}
 	sortAlltime() {
+		if (this.state.alltimeArr.length===0) return;
 		var arr = [];
 		if (this.state.alltimeOrder===0) {
-			arr = this.state.arr.sort( (a,b) => a.alltime-b.alltime);
+			arr = this.state.alltimeArr.sort( (a,b) => a.alltime-b.alltime);
 			this.setState({'alltimeOrder': 1});
 		}
 		else
 		{
-			arr = this.state.arr.sort( (a,b) => b.alltime-a.alltime);
+			arr = this.state.alltimeArr.sort( (a,b) => b.alltime-a.alltime);
 			this.setState({'alltimeOrder': 0});
 		}
-		this.setState({'arr': arr});
+		this.setState({'alltimeArr': arr});
 		this.setRows(arr);
 	}
 	setRows(json){
@@ -65,6 +66,9 @@ class Table extends React.Component {
 		$.getJSON(recentLink, json => {
 			this.setState({'arr': json});
 			this.setRows(json);
+			$.getJSON(allTimeLink, json => {
+				this.setState({'alltimeArr': json});
+			});
 		});										
 	}
 	render() {
@@ -76,8 +80,8 @@ class Table extends React.Component {
 					<tr>
 						<th>#</th>
 						<th>Camper Name</th>
-						<th onClick={this.sortRecent}>Points in past 30 days <i className="sort-arrow">{(this.state.recentOrder) ? '↑' : '↓'}</i></th>
-						<th onClick={this.sortAlltime}>All time points <i className="sort-arrow">{(this.state.alltimeOrder) ? '↑' : '↓'}</i></th>
+						<th onClick={this.sortRecent}>Points in past 30 days <i className="sort-arrow">{ (this.state.arr.length>0) ? (this.state.recentOrder) ? '↑' : '↓' : '' }</i></th>
+						<th onClick={this.sortAlltime}>All time points <i className="sort-arrow">{ (this.state.alltimeArr.length>0) ? (this.state.alltimeOrder) ? '↑' : '↓' : ''}</i></th>
 					</tr>
 				</thead>
 				<tbody>
